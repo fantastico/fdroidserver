1411e03caa4f118dcfddb2c60b6203c800d29c8b
lijiyang <ken.li@sen5.com>
2014-05-14 14:54:27 +0800

    Merge remote-tracking branch 'remotes/remote/master' into fdroidserver_lijiyang
    Conflicts:
    	fdroidserver/common.py

---
+++ sen5/dbConfig.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+__author__ = 'lijiyang'
+
+host = '192.168.0.8'
+port = 27017
+db = 'sen5_app_store'
+apps = 'apps'
--- sen5/saveIndex.py
+++ sen5/saveIndex.py
@@ -2,4 +2,25 @@
 # -*- coding: utf-8 -*-
 __author__ = 'lijiyang'
 
+from pymongo import MongoClient
 
+import dbConfig
+
+
+class Sen5DB:
+    client = None
+
+    def __init__(self, max_ps=100):
+        if Sen5DB.client is None:
+            Sen5DB.client = MongoClient(dbConfig.host, dbConfig.port, max_pool_size=max_ps)
+        self.database = Sen5DB.client[dbConfig.db]
+        self.apps = self.database[dbConfig.apps]
+
+
+def main():
+    db = Sen5DB(1)
+    db.apps.insert()
+
+
+if __name__ == "__main__":
+    main()
