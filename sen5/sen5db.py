9567c0eed1f76e22d36d76036eed5658c1298fa0
lijiyang <ken.li@sen5.com>
2014-05-20 17:16:35 +0800

    insert scores

--- sen5/sen5db.py
+++ sen5/sen5db.py
@@ -18,13 +18,15 @@ class Sen5DB:
         self.database = Sen5DB.client[config.db]
         self.apps = self.database['apps']
         self.comments = self.database['comments']
+        self.scores = self.database['scores']
 
     def clear(self):
         self.apps.drop()
         self.comments.drop()
+        self.scores.drop()
         self.apps = self.database['apps']
         self.comments = self.database['comments']
-
+        self.scores = self.database['scores']
 
     def format(self, apps):
         result = []
@@ -32,13 +34,21 @@ class Sen5DB:
             app = app['application']
             app['name'] = {'default': app['name']}
             app['desc'] = {'default': app['desc']}
-            app['comments'] = self.comments.insert({})
+            app['comments'] = self.init_comments(app['id'])
+            app['scores'] = self.init_scores(app['id'])
             result.append(app)
         return result
 
+    def init_comments(self, appid):
+        return self.comments.insert({'_id': appid})
+
+    def init_scores(self, appid):
+        return self.scores.insert(
+            {'_id': appid, 'user_friendly': 0, 'easy_to_use': 0, 'utility': 0, 'downloads': 0, 'comment': 0,
+             'marking': 0})
 
     def save(self, apps):
-            #db.apps.update({'id': app_body['id']}, app_body, upsert=True)
+    #db.apps.update({'id': app_body['id']}, app_body, upsert=True)
         self.apps.insert(apps)
 
     def update(self):
