b63dd4ca56913aa7c5a8845ffa023dad326cf384
ken <testlee@126.com>
2015-01-16 12:53:09 +0800

    fix bug: fail to add apk to app entry

--- sen5/sen5db.py
+++ sen5/sen5db.py
@@ -6,57 +6,25 @@ from pymongo import MongoClient
 import xml2json
 import config
 
-
-class Sen5DB:
-    client = None
-
-    """ input: max_ps, maximum number of connections in the pool """
-
-    def __init__(self, max_ps=20):
-        if Sen5DB.client is None:
-            Sen5DB.client = MongoClient(config.host, config.port, max_pool_size=max_ps)
-        self.database = Sen5DB.client[config.db]
-        self.apps = self.database['apps']
-        self.comments = self.database['comments']
-        self.scores = self.database['scores']
-
-    def clear(self):
-        self.apps.drop()
-        self.comments.drop()
-        self.scores.drop()
-        self.apps = self.database['apps']
-        self.comments = self.database['comments']
-        self.scores = self.database['scores']
-
-    def format(self, apps):
-        result = []
-        for app in apps:
-            app = app['application']
-            app['name'] = {'default': app['name']}
-            app['desc'] = {'default': app['desc']}
-            app['comments'] = self.init_comments(app['id'])
-            app['scores'] = self.init_scores(app['id'])
-            result.append(app)
-        return result
-
-    def init_comments(self, appid):
-        return self.comments.insert({'_id': appid, 'comments': []})
-
-    def init_scores(self, appid):
-        return self.scores.insert(
-            {'_id': appid, 'user_friendly': 0, 'easy_to_use': 0, 'utility': 0, 'downloads': 0, 'comment': 0,
-             'marking': 0})
-
-    def save(self, apps):
-        self.apps.insert(apps)
-
-    def update(self):
-        self.clear()
-        apps = xml2json.getJson('application')
-        if len(apps) > 0:
-            apps = self.format(apps)
-            self.save(apps)
-
+score = {
+    '_id': '',
+    'score': 0,
+    'downloads': 0,
+    'comment': 0,
+    'marking': 0,
+    'pictures': [
+        'http://192.168.0.8/kenrepo/icons-640/com.sen5.android.remoteServer.20140930.png',
+        'http://192.168.0.8/kenrepo/icons-640/com.sodao.12.png',
+        'http://192.168.0.8/kenrepo/icons-640/com.sen5.android.remoteServer.20140930.png',
+        'http://192.168.0.8/kenrepo/icons-640/com.sodao.12.png',
+        'http://192.168.0.8/kenrepo/icons-640/com.sen5.android.remoteServer.20140930.png',
+        'http://192.168.0.8/kenrepo/icons-640/com.sodao.12.png',
+        'http://192.168.0.8/kenrepo/icons-640/com.sen5.android.remoteServer.20140930.png',
+        'http://192.168.0.8/kenrepo/icons-640/com.sodao.12.png',
+        'http://192.168.0.8/kenrepo/icons-640/com.sen5.android.remoteServer.20140930.png',
+        'http://192.168.0.8/kenrepo/icons-640/com.sodao.12.png'
+    ]
+}
 
 common_repository = {
     '_id': 'common',
@@ -75,6 +43,8 @@ class Sen5AppsDB:
         self.apps = self.database['apps']
         self.repositories = self.database['repositories']
         self.main_repo = self.database['main_repo']
+        self.comments = self.database['comments']
+        self.scores = self.database['scores']
 
     def check_app_group_exist(self, app_search_condition):
         result = self.repositories.find_one(app_search_condition, {'_id': 1})
@@ -98,17 +68,66 @@ class Sen5AppsDB:
     def add_apk(self, _id, apk):
         self.apps.update({'_id': _id}, {'$push': {'package': apk}})
 
+    def clear(self):
+        self.apps.drop()
+        self.comments.drop()
+        self.scores.drop()
+        self.apps = self.database['apps']
+        self.comments = self.database['comments']
+        self.scores = self.database['scores']
+
+    def format(self, apps):
+        result = []
+        for app in apps:
+            app = app['application']
+            app['name'] = {'default': app['name']}
+            app['desc'] = {'default': app['desc']}
+            app['comments'] = self.init_comments(app['id'])
+            app['scores'] = self.add_scores(app['id'])
+            result.append(app)
+        return result
+
+    def init_comments(self, appid):
+        return self.comments.insert({'_id': appid, 'comments': []})
+
+    def add_scores(self, appid):
+        score['_id'] = appid
+        return self.scores.insert(score)
+
+    def save(self, apps):
+        self.apps.insert(apps)
+
+    def update(self):
+        self.clear()
+        apps = xml2json.getJson('application')
+        if len(apps) > 0:
+            apps = self.format(apps)
+            self.save(apps)
+
+    def init_scores(self):
+        ids = self.apps.find({}, {'_id': 1})
+        for app_id in ids:
+            result = self.scores.find_one({'_id': app_id}, {'_id': 1})
+            if result is None:
+                score['_id'] = app_id['_id']
+                self.scores.insert(score)
+                print 'init scores for app id=' + str(app_id['_id'])
+
 
 def update_db():
-    # db = Sen5DB(1)
-    # db.update()
     db = Sen5AppsDB()
-    result = db.create_common_repository()
-    print result
+    db.update()
+    # result = db.create_common_repository()
+    # print result
+
+
+# import pydevd
+# pydevd.settrace('192.168.0.123', port=51234, stdoutToServer=True, stderrToServer=True)
 
 
 def main():
-    update_db()
+    db = Sen5AppsDB()
+    db.init_scores()
 
 
 if __name__ == "__main__":
